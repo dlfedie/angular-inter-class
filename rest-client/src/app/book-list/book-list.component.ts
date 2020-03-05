b4128d01b18cf9f5f5d0c3f243235bcb7536b087
Dustin Fedie <dlfedie@gmail.com>
2020-03-05 08:52:21 -0800

    adds subscription to bookList component

--- rest-client/src/app/book-list/book-list.component.ts
+++ rest-client/src/app/book-list/book-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService, Book } from '../data.service';
+
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
+  books:Book[] = [];
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getBooks().subscribe(bookList => {
+      this.books = bookList
+    })
   }
 
 }
