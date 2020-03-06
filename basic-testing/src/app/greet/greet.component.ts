8ee144713b043bf9f9ce75c72b0c4e0493727b7a
Dustin Fedie <dlfedie@gmail.com>
2020-03-06 11:42:31 -0800

    adds some inputs for testing the add method

--- basic-testing/src/app/greet/greet.component.ts
+++ basic-testing/src/app/greet/greet.component.ts
@@ -9,6 +9,10 @@ import { SimpleService } from '../simple.service';
 export class GreetComponent implements OnInit {
   userName:string = '';
 
+  numberA = 0
+  numberB = 0
+  addResult = 0
+
   constructor(private service: SimpleService) { }
 
   ngOnInit() {
@@ -16,6 +20,11 @@ export class GreetComponent implements OnInit {
 
   getGreeting() : string {
     return this.service.sayHello(this.userName)
+  }
+
+  add() {
+    this.service.addNumbers(this.numberA, this.numberB)
+      .subscribe((result) => this.addResult = result)
   }
 
 }
