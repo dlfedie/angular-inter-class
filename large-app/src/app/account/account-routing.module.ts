262e390330016e438b1524546c4c98045293a7f5
Dustin Fedie <dlfedie@gmail.com>
2020-03-06 07:14:36 -0800

    adds routes to account comp

--- large-app/src/app/account/account-routing.module.ts
+++ large-app/src/app/account/account-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AccountHomeComponent } from './account-home/account-home.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
+import { AddressbookComponent } from './addressbook/addressbook.component';
+
+const routes: Routes = [
+  { path: "", component: AccountHomeComponent },
+  { path: "orders", component: OrderHistoryComponent },
+  { path: "address", component: AddressbookComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
