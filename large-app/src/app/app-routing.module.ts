3edea9e409070b4454d186fc312f0c3b4e62984c
Dustin Fedie <dlfedie@gmail.com>
2020-03-06 07:18:19 -0800

    adds main routing

--- large-app/src/app/app-routing.module.ts
+++ large-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CatalogComponent } from './catalog/catalog.component';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "catalog", component: CatalogComponent },
+  { path: "account", loadChildren: "./account/account.module#AccountModule"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
