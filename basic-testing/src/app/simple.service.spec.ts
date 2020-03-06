6e4968e949ca8146d6e9ef263a11ae39896a84e5
Dustin Fedie <dlfedie@gmail.com>
2020-03-06 11:12:36 -0800

    sets up hello test

--- basic-testing/src/app/simple.service.spec.ts
+++ basic-testing/src/app/simple.service.spec.ts
@@ -3,10 +3,22 @@ import { TestBed } from '@angular/core/testing';
 import { SimpleService } from './simple.service';
 
 describe('SimpleService', () => {
+  let service: SimpleService;
   beforeEach(() => TestBed.configureTestingModule({}));
+
+  beforeEach(() => {
+    service = TestBed.get(SimpleService)
+  })
 
   it('should be created', () => {
     const service: SimpleService = TestBed.get(SimpleService);
     expect(service).toBeTruthy();
   });
+
+  it('should call sayHello', () => {
+    let name = 'Bob';
+    let greeting = service.sayHello(name)
+
+    expect(greeting).toBe(`Hello $(name)`)
+  })
 });
